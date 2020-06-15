240888f2401553c7ef6d028ef6094e6e527d694e
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-06-16 00:01:40 +0300

    Still work with Django framework. Started using basic testing

--- newtechnologies/articles/views.py
+++ newtechnologies/articles/views.py
@@ -2,34 +2,51 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'articles/index.html', context)
+# def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {
+#        'latest_question_list': latest_question_list,
+#    }
+#    return render(request, 'articles/index.html', context)
+
+class IndexView(generic.ListView):
+    template_name = 'articles/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'articles/detail.html'
+
+
+#def detail(request, question_id):
     # question = get_object_or_404(Question, pk=question_id)
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist!")
-    return render(request, 'articles/detail.html', {'question': question})
+#    try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404("Question does not exist!")
+#    return render(request, 'articles/detail.html', {'question': question})
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'articles/results.html'
 
-def results(request, question_id):
-    response = "You are looking at the results of question %s."
-    return HttpResponse(response % question_id)
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'articles/results.html', {'question':question})
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pr=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'articles/detail.html',
                       {
